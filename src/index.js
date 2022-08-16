1e5dbf4c297f9b70e358dcb9e989d09fbddbdb08
guguducken <1173781832@qq.com>
2022-08-16 23:58:06 +0800

    turn milestones to false

--- src/index.js
+++ src/index.js
@@ -7,15 +7,13 @@ const milestones = core.getInput("milestones");
 async function run() {
     const context = github.context;
 
-    const issue_action = context.issue;
 
     const oc = github.getOctokit(github_token);
 
     const { data: issue } = await oc.rest.issues.get(
         {
-            owner: issue_action.owner,
-            repo: issue_action.repo,
-            issue_number: issue_action.number
+            ...context.repo,
+            issue_number: context.issue.number
         }
     )
     core.info(JSON.stringify(issue));
