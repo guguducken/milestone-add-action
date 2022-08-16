c9250d769a7c085dae1602dbd18664ac19faf885
guguducken <1173781832@qq.com>
2022-08-16 23:45:22 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const github_token = core.getInput("action-token", { required: true });
+const milestones = core.getInput("milestones");
+
+async function run() {
+    const context = github.context;
+
+    const oc = github.getOctokit(github_token);
+
+    const { data: issue } = await oc.rest.issues.get(
+        context.issue,
+    )
+    core.info(JSON.stringify(issue));
+}
+
+run();
