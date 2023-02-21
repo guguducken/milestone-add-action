c025acd559ee8ac4ed4879d67b1ec495836d9497
guguducken <117371832@qq.com>
2023-02-21 10:55:23 +0800

    adjust the struct of code

--- src/index.js
+++ src/index.js
@@ -6,9 +6,36 @@ const milestone = core.getInput("milestone", { required: true });
 
 async function run() {
     try {
+        if (milestone.length == 0) {
+            throw new Error("There is no corresponding milestones,please check your yml file or issue title");
+        }
+
         const context = github.context;
 
         const oc = github.getOctokit(github_token);
+
+        //get milestones of repository
+        const { data: repo_milestones } = await oc.rest.issues.listMilestones(
+            {
+                ...context.repo,
+            }
+        )
+
+        if (repo_milestones.length == 0) {
+            throw new Error("The repository " + context.repo.repo + "don't have any milestone");
+        }
+        //check whether milestone exists and get the number of target milestone
+        let target = -1;
+        for (const repo_mile of repo_milestones) {
+            if (targetName == repo_mile.title) {
+                target = repo_mile.number;
+                break;
+            }
+        }
+
+        if (target == -1) {
+            throw new Error("There is no corresponding Milestone in the repository");
+        }
 
         //get issue detail
         const { data: issue } = await oc.rest.issues.get(
@@ -28,35 +55,7 @@ async function run() {
             core.info("The issue #" + context.issue.number + " already have milestone: " + issue_milestone.title);
             return;
         }
-
-        if (milestone.length == 0) {
-            throw new Error("There is no corresponding milestones,please check your yml file or issue title");
-        }
         core.info("The target milestone which need to add to this issue is: " + milestone);
-
-        //get milestones of repository
-        const { data: repo_milestones } = await oc.rest.issues.listMilestones(
-            {
-                ...context.repo,
-            }
-        )
-
-        if (repo_milestones.length == 0) {
-            throw new Error("The repository " + context.repo.repo + "don't have any milestone");
-        }
-
-        //check whether milestone exists and get the number of target milestone
-        let target = -1;
-        for (const repo_mile of repo_milestones) {
-            if (targetName == repo_mile.title) {
-                target = repo_mile.number;
-                break;
-            }
-        }
-
-        if (target == -1) {
-            throw new Error("There is no corresponding Milestone in the repository");
-        }
 
         //add milestone to issue
         await oc.rest.issues.update(
@@ -66,7 +65,6 @@ async function run() {
                 milestone: target,
             }
         )
-
     } catch (err) {
         core.setFailed(err.message);
     }
